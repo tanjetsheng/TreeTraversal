80e96c68f0956b5d3a68e65db0f987df7c03423c
tanjetsheng <tanjetsheng@gmail.com>
2017-10-29 17:02:47 +0800

    modified

--- src/NodePrint.c
+++ src/NodePrint.c
@@ -2,13 +2,13 @@
 #include "NodePrint.h"
 
 
-
-
 void printInteger(Node *node){
-  printf("(**)%d,",node->data);
+    printf("%d, ",node->data);
 }
 
+void printDecoratedInteger(Node *node){
+    printf("(***)%d, ",node->data);
+}
 
 void printString(Node *node){
-  printf("%s,",node->data);
-}
+    printf("%s, ",node->data);
--- src/NodePrint.h
+++ src/NodePrint.h
@@ -3,5 +3,6 @@
 #include "Node.h"
 
 void printInteger(Node *node);
+void printDecoratedInteger(Node *node);
 void printString(Node *node);
 #endif // _NODEPRINT_H
